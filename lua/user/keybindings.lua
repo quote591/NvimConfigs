fb3233d217dcba8da8bd3dd6fac0a9c6e59efd84
Matt Bannister <matt.bann@live.co.uk>
2025-08-18 21:51:16 +0100

    Change the keybindings

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -1,6 +1,6 @@
 
 -- Horizontal and verticle split
-vim.keymap.set("n", "<leader>hs", "<CMD>:split<CR>", { desc = "Open horizontal split" })
-vim.keymap.set("n", "<leader>vs", "<CMD>:vsplit<CR>", { desc = "Open verticle split" })
+vim.keymap.set({"n", "i", "v"}, "<C-p>", "<CMD>:split<CR>", { desc = "Open horizontal split" })
+vim.keymap.set({"n", "i", "v"}, "<C-o>", "<CMD>:vsplit<CR>", { desc = "Open verticle split" })
 
 
